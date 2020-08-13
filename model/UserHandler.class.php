e10a55d052f2180d7c4f9d6bf14ba3de45d71998
Eatfire <97473uk@saeinstitute.edu>
2020-08-13 07:54:12 +0100

    Updated ReadMe.PDF

--- model/UserHandler.class.php
+++ model/UserHandler.class.php
@@ -14,6 +14,7 @@ class UserHandler extends User
     public function Get_user_info_by_id_Handler($id)
     {
         $UserHandler = $this->Get_user_info_by_id($id);
+
         return $UserHandler;
     }
 
